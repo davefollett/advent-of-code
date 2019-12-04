eb90d5dc79d867184f1542bbcad08f508a871dd6
Dave Follett <davefollett@gmail.com>
2019-12-03 23:47:12 -0500

    refactoring repeated code into a function

--- 2019/day-03/index.js
+++ 2019/day-03/index.js
@@ -20,13 +20,9 @@ function getDistance(point1X, point1Y, point2X, point2Y) {
   return Math.abs(point2X - point1X) + Math.abs(point2Y - point1Y);
 }
 
-function initWires() {
-  let wireOne = new Map();
-  let wireTwo = new Map();
+function traceWire(path) {
+  let wire = new Map();
 
-  let wires = fs.readFileSync("./day-03/input.txt", 'utf-8').split(require('os').EOL)
-  //let wires = fs.readFileSync("./day-03/test.txt", 'utf-8').split(require('os').EOL)
-  
   let move = {
     "R": {x: 1, y: 0},
     "L": {x: -1, y: 0},
@@ -37,7 +33,7 @@ function initWires() {
   let point = { x: 0, y: 0 };
   let steps = 0;
 
-  wires[0].split(',').forEach(function(movement) {
+  path.split(',').forEach(function(movement) {
     let direction = movement[0]
     let distance = parseInt(movement.split(direction)[1])
 
@@ -47,29 +43,22 @@ function initWires() {
         y: point.y += move[direction].y }
 
       steps += 1;
-      wireOne.set(`${newPoint.x},${newPoint.y}`, steps);
+      wire.set(`${newPoint.x},${newPoint.y}`, steps);
       point.x = newPoint.x;
       point.y = newPoint.y;
     }
   })
 
-  point = { x: 0, y: 0 };
-  steps = 0;
+  return wire;
+}
 
-  wires[1].split(',').forEach(function(movement) {
-    let direction = movement[0]
-    let distance = parseInt(movement.split(direction)[1])
+function initWires() {
 
-    for(let i = 0; i < distance; i++) {
-      let newPoint = {
-        x: point.x + move[direction].x,
-        y: point.y += move[direction].y }
-
-      steps += 1;
-      wireTwo.set(`${newPoint.x},${newPoint.y}`, steps);
-      point = newPoint;
-    }
-  })
+  let wires = fs.readFileSync("./day-03/input.txt", 'utf-8').split(require('os').EOL)
+  //let wires = fs.readFileSync("./day-03/test.txt", 'utf-8').split(require('os').EOL)
+  
+  let wireOne = traceWire(wires[0]);
+  let wireTwo = traceWire(wires[1]);
 
   return {wireOne, wireTwo};
 }
