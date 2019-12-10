9b1aa54dcda0b10bc2a71a68d3343f8737b6cc13
Dave Follett <davefollett@gmail.com>
2019-12-09 22:14:46 -0500

    Day 6 part 1 is working.

--- 2019/day-06/index.js
+++ 2019/day-06/index.js
@@ -15,14 +15,11 @@ let results = {
   }
 };
 
-
 function countOrbits(orbitTree, current) {
 
   let result = 0;
 
-  if(current.parent === null) {
-    return result + 1;
-  }
+  if(current.parent === null) { return result; }
 
   return countOrbits(orbitTree, orbitTree[current.parent]) + 1;
 }
@@ -37,79 +34,16 @@ function part1(input) {
     }
   };
 
-  let orbits = input
-    .split(require('os').EOL)
-    .map(line => {
-      const [parent, child] = line.split(")");
-      return {parent: parent, child: child}
-    });
+  input.split(require('os').EOL).forEach(line => {
+    const [parent, child] = line.split(")");
+    orbitTree[child] = {parent: parent};
+  });
 
-  console.log(orbits)
+  //console.log(orbitTree);
 
-  let parent = "COM";
-  //let index = 0;
-  let child = ""
-
-  while(orbits.length !== 0) {
-    //console.log("here")
-    
-
-    // let child = ""
-
-    let index = orbits.findIndex(element => {
-      //console.log(element)
-      return element.parent === parent;
-    });
-
-    if(index !== -1) {
-      //console.log("found it");
-      child = orbits[index].child;
-      parent = orbits[index].parent;
-
-      orbitTree[child] = {
-        parent: parent
-      }
-    //   console.log(orbitTree);
-    //   result += countOrbits(orbitTree, orbitTree[parent]);
-       orbits.splice(index, 1);
-
-    } else {
-      //console.log("no found it");
-      parent = child;
-    }
-    //orbits.splice(index, 1);
-    console.log(orbits.length)
+  for (let key in orbitTree) {
+    result += countOrbits(orbitTree, orbitTree[key]);
   }
-  console.log(orbitTree);
-
-  // let COM = orbits.filter(function(orbit, index, node) {
-  //   console.log(this.value)
-  //   return orbit.parent === this;
-  // });
-
-  // console.log(COM)
-
-
-  // while(orbits.length !== 0) {
-
-  //   orbits
-
-
-  // }
-
-
-
-  //   .forEach(line => {
-  //     const [parent, child] = line.split(")");
-      
-  //     orbitTree[child] = {
-  //       parent: parent,
-  //     }
-  //     result += countOrbits(orbitTree, orbitTree[parent]);
-  //     //console.log(orbitTree);
-  //   });
-
-    //console.log(orbitTree);
 
   return result;
 }
@@ -134,30 +68,3 @@ exports.run = function run() {
 
   return results;
 };
-
-// function part1(input) {
-
-//   let result = 0;
-
-//   orbitTree = {
-//     "COM": {
-//       parent: null,
-//     }
-//   };
-
-//   let orbits = input
-//     .split(require('os').EOL)
-//     .forEach(line => {
-//       const [parent, child] =line.split(")");
-      
-//       orbitTree[child] = {
-//         parent: parent,
-//       }
-//       result += countOrbits(orbitTree, orbitTree[parent]);
-//       //console.log(orbitTree);
-//     });
-
-//     //console.log(orbitTree);
-
-//   return result;
-// }
--- 2019/day-06/index.test.js
+++ 2019/day-06/index.test.js
@@ -3,14 +3,12 @@ const day06 = require("./index.js");
 
 test('Part 1 run(TEST)', () => {
 
-//   let input = fs.readFileSync("./day-06/test.txt", 'utf-8');
-  
-//   expect(day06.part1(input)).toBe(42);
+  let input = fs.readFileSync("./day-06/test.txt", 'utf-8');
+  expect(day06.part1(input)).toBe(42);
 });
 
-// test('Part 1 run(REAL)', () => {
+test('Part 1 run(REAL)', () => {
 
-//   let input = fs.readFileSync("./day-06/input.txt", 'utf-8');
-//   console.log(day06.part1(input));
-//   //expect(day06.part1(input)).toBe(42);
-// });
+  let input = fs.readFileSync("./day-06/input.txt", 'utf-8');
+  expect(day06.part1(input)).toBe(154386);
+});
