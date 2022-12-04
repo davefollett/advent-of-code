16843fc3ecc012587263ce7e3cd7d08407cf56d8
Dave Follett <davefollett@gmail.com>
2022-12-04 00:57:22 -0500

    2022 - Day 04 parts 1 & 2 solutions.

--- 2022/javascript/day-04/index.js
+++ 2022/javascript/day-04/index.js
@@ -5,21 +5,47 @@ import Result from '../utils/result.js';
 import fileParser from '../utils/file-parser.js';
 
 function lineParserP1(line) {
-  return line.length;
+  const [e1s, e1e, e2s, e2e] = line.split(/[-,]+/);
+
+  const elf1Start = parseInt(e1s, 10);
+  const elf1End = parseInt(e1e, 10);
+  const elf2Start = parseInt(e2s, 10);
+  const elf2End = parseInt(e2e, 10);
+
+  if ((elf1Start >= elf2Start && elf1End <= elf2End)
+  || (elf2Start >= elf1Start && elf2End <= elf1End)) {
+    return 1;
+  }
+
+  return 0;
 }
 
 export function part1(filename) {
-  const data = fileParser(filename, lineParserP1);
-  return data.length;
+  const completeOverlaps = fileParser(filename, lineParserP1);
+  return completeOverlaps.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 }
 
 function lineParserP2(line) {
-  return line.length;
+  const [e1s, e1e, e2s, e2e] = line.split(/[-,]+/);
+
+  const elf1Start = parseInt(e1s, 10);
+  const elf1End = parseInt(e1e, 10);
+  const elf2Start = parseInt(e2s, 10);
+  const elf2End = parseInt(e2e, 10);
+
+  if ((elf1Start >= elf2Start && elf1Start <= elf2End)
+  || (elf2Start >= elf1Start && elf2Start <= elf1End)
+  || (elf1End >= elf2Start && elf1End <= elf2End)
+  || (elf2End >= elf1Start && elf2End <= elf1End)) {
+    return 1;
+  }
+
+  return 0;
 }
 
 export function part2(filename) {
-  const data = fileParser(filename, lineParserP2);
-  return data.length;
+  const anyOverlaps = fileParser(filename, lineParserP2);
+  return anyOverlaps.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 }
 
 export function run() {
--- 2022/javascript/day-04/index.test.js
+++ 2022/javascript/day-04/index.test.js
@@ -10,24 +10,24 @@ describe('@/day-04/index.js', () => {
   describe('part1()', () => {
     it('part1 should pass', () => {
       const answer = part1(inputFilename);
-      expect(answer).toBe(1);
+      expect(answer).toBe(494);
     });
 
     it('part1 should pass test input', () => {
       const answer = part1(testInputFilename);
-      expect(answer).toBe(1);
+      expect(answer).toBe(2);
     });
   });
 
   describe('part2()', () => {
     it('part2 should pass', () => {
       const answer = part2(inputFilename);
-      expect(answer).toBe(1);
+      expect(answer).toBe(833);
     });
 
     it('part2 should pass test input', () => {
       const answer = part2(testInputFilename);
-      expect(answer).toBe(1);
+      expect(answer).toBe(4);
     });
   });
 });
