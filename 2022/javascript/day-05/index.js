de1df1710378e0fd3710f8eb499751f04afa6726
Dave Follett <davefollett@gmail.com>
2022-12-05 01:55:28 -0500

    2022 - Day 05 part 1 & 2 solutoins.  Needs a bit of cleanup though.

--- 2022/javascript/day-05/index.js
+++ 2022/javascript/day-05/index.js
@@ -5,21 +5,104 @@ import Result from '../utils/result.js';
 import fileParser from '../utils/file-parser.js';
 
 function lineParserP1(line) {
-  return line.length;
+  return line;
 }
+
+function separate(data) {
+  let separatorFound = false;
+  const stacks = [];
+  const moves = [];
+
+  const numStacks = (data[0].length + 1) / 4;
+
+  for (let i = 0; i < numStacks; i++) {
+    stacks.push([]);
+  }
+
+  data.forEach((line) => {
+    if (line.length === 0) {
+      separatorFound = true;
+      return;
+    }
+
+    if(separatorFound) {
+      moves.push(line);
+    } else {
+      if (line.includes('1')) { return; }
+
+      for (let k = 1, i = 0; k <= line.length; k += 4, i++) {
+        if (line[k] !== ' ') {
+          stacks[i].push(line[k]);
+        }
+      }
+    }
+  });
+
+  stacks.forEach((stack) => stack.reverse());
+
+  return {stacks, numStacks, moves};
+}
+
 
 export function part1(filename) {
   const data = fileParser(filename, lineParserP1);
-  return data.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+  
+  const {stacks, numStacks, moves} = separate(data);
+  // console.log(stacks)
+  // console.log(moves)
+  // console.log(numStacks)
+
+  moves.forEach((move) => {
+    const [ , amountStr,, srcStackStr,, destStackStr ] = move.split(' ');
+    const amount = parseInt(amountStr, 10);
+    const srcStack = parseInt(srcStackStr, 10);
+    const destStack = parseInt(destStackStr, 10);
+    // console.log(amount, srcStack, destStack)
+    for (let i = 0; i < amount; i++) {
+      const crate = stacks[srcStack-1].pop();
+      stacks[destStack-1].push(crate);
+    }
+  })
+
+  // console.log(stacks)
+
+  let result = '';
+  for (let i = 0; i < numStacks; i++) {
+    result += stacks[i].pop();
+  }
+
+  return result;
 }
 
 function lineParserP2(line) {
-  return line.length;
+  return line;
 }
 
 export function part2(filename) {
   const data = fileParser(filename, lineParserP2);
-  return data.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+  const {stacks, numStacks, moves} = separate(data);
+  // console.log(stacks)
+  moves.forEach((move) => {
+    const [ , amountStr,, srcStackStr,, destStackStr ] = move.split(' ');
+    const amount = parseInt(amountStr, 10);
+    const srcStack = parseInt(srcStackStr, 10);
+    const destStack = parseInt(destStackStr, 10);
+    // console.log(amount, srcStack, destStack)
+
+    const crates = stacks[srcStack-1].splice(stacks[srcStack-1].length - amount, amount);
+    // console.log(amount, crates)
+    stacks[destStack-1] = stacks[destStack-1].concat(crates);
+    // console.log(stacks)
+  })
+
+  
+
+  let result = '';
+  for (let i = 0; i < numStacks; i++) {
+    result += stacks[i].pop();
+  }
+
+  return result;
 }
 
 export function run() {
--- 2022/javascript/day-05/index.test.js
+++ 2022/javascript/day-05/index.test.js
@@ -8,26 +8,26 @@ const testInputFilename = './day-05/test-input.txt';
 
 describe('@/day-05/index.js', () => {
   describe('part1()', () => {
-    // it('part1 should pass', () => {
-    //   const answer = part1(inputFilename);
-    //   expect(answer).toBe(494);
-    // });
+    it('part1 should pass', () => {
+      const answer = part1(inputFilename);
+      expect(answer).toBe('PTWLTDSJV');
+    });
 
     it('part1 should pass test input', () => {
       const answer = part1(testInputFilename);
-      expect(answer).toBe(0);
+      expect(answer).toBe('CMZ');
     });
   });
 
   describe('part2()', () => {
-  //   it('part2 should pass', () => {
-  //     const answer = part2(inputFilename);
-  //     expect(answer).toBe(833);
-  //   });
+    it('part2 should pass', () => {
+      const answer = part2(inputFilename);
+      expect(answer).toBe('WZMFVGGZP');
+    });
 
     it('part2 should pass test input', () => {
       const answer = part2(testInputFilename);
-      expect(answer).toBe(0);
+      expect(answer).toBe('MCD');
     });
   });
 });
