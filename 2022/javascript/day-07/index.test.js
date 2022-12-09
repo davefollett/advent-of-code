4c6ebb87a963006a1b2ee33c7a549702a255a6a1
Dave Follett <davefollett@gmail.com>
2022-12-09 09:55:29 -0500

    2022 - Day 07 part 1 solution works but the code needs a good amount of cleanup.

--- 2022/javascript/day-07/index.test.js
+++ 2022/javascript/day-07/index.test.js
@@ -11,24 +11,24 @@ describe('@/day-07/index.js', () => {
   describe('part1()', () => {
     it('part1 should pass', () => {
       const answer = part1(inputFilename);
-      expect(answer).toBe(0);
+      expect(answer).toBe(1390824);
     });
 
     it('part1 should pass test input', () => {
       const answer = part1(testInputFilename);
-      expect(answer).toBe(0);
+      expect(answer).toBe(95437);
     });
   });
 
-  describe('part2()', () => {
-    it('part2 should pass', () => {
-      const answer = part2(inputFilename);
-      expect(answer).toBe(0);
-    });
+  // describe('part2()', () => {
+  //   it('part2 should pass', () => {
+  //     const answer = part2(inputFilename);
+  //     expect(answer).toBe(0);
+  //   });
 
-    it('part2 should pass test input', () => {
-      const answer = part2(testInputFilename);
-      expect(answer).toBe(0);
-    });
-  });
+  //   it('part2 should pass test input', () => {
+  //     const answer = part2(testInputFilename);
+  //     expect(answer).toBe(0);
+  //   });
+  // });
 });
