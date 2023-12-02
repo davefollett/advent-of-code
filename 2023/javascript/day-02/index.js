f39a2f581a65a0763db6c35b6c5468b50b4758e3
Dave Follett <davefollett@gmail.com>
2023-12-02 15:10:21 -0500

    2023 Day 02: Part 1 & 2 working.

--- 2023/javascript/day-02/index.js
+++ 2023/javascript/day-02/index.js
@@ -1,26 +1,103 @@
-// https://adventofcode.com/2023/day/1
+// https://adventofcode.com/2023/day/2
 
 import { performance } from 'node:perf_hooks';
 import Result from '../utils/result.js';
 import fileParser from '../utils/file-parser.js';
 import { sumNumbers } from '../utils/array.js';
 
-function lineParserP1(line) {
-  return line;
+const limits = {
+  red: 12,
+  green: 13,
+  blue: 14
 }
 
-function lineParserP2(line) {
-  return line;
+function debugCubeTotals(totals) {
+  console.log(`red: ${totals['red']} green: ${totals['green']} blue: ${totals['blue']}`)
 }
+
+function lineParserP1(line) {
+  let validPulls = 0;
+  const gameNumber = parseInt(line.split(' ')[1].split(':')[0], 10);
+  const pulls = line.split(':')[1].split(';').map((game) => game.trim())
+  for (const pull of pulls) {
+    const cubeTotals = {
+      red: 0,
+      green: 0,
+      blue: 0,
+    }
+    // console.log('pull', pull)
+    const cubes = pull.split(',');
+    for (const cube of cubes) {
+      const cubeTrimmed = cube.trim()
+      // console.log('cube', cube)
+      const number = parseInt(cubeTrimmed.split(' ')[0].trim(), 10);
+      const color = cubeTrimmed.split(' ')[1].trim();
+      cubeTotals[color] += number;
+      // console.log(number, color)
+    }
+    // debugCubeTotals(cubeTotals)
+    // debugCubeTotals(limits)
+    if (cubeTotals['red'] <= limits['red']
+    && cubeTotals['green'] <= limits['green']
+    && cubeTotals['blue'] <= limits['blue']) {
+      // console.log('HERERERERERE')
+      validPulls += 1;
+    }
+  }
+  // console.log(pulls)
+  // console.log(validPulls)
+  return (validPulls === pulls.length) ? gameNumber : 0;
+}
+
 
 export function part1(filename) {
   const lines = fileParser(filename, lineParserP1);
-  return 0;
+  return sumNumbers(lines);
 }
+
+function lineParserP2(line) {
+  // let validPulls = 0;
+  const smallestLimit = {
+    red: 0,
+    green: 0,
+    blue: 0,
+  }
+
+  const gameNumber = parseInt(line.split(' ')[1].split(':')[0], 10);
+  const pulls = line.split(':')[1].split(';').map((game) => game.trim())
+  for (const pull of pulls) {
+    const cubeTotals = {
+      red: 0,
+      green: 0,
+      blue: 0,
+    }
+    // console.log('pull', pull)
+    const cubes = pull.split(',');
+    for (const cube of cubes) {
+      const cubeTrimmed = cube.trim()
+      // console.log('cube', cube)
+      const number = parseInt(cubeTrimmed.split(' ')[0].trim(), 10);
+      const color = cubeTrimmed.split(' ')[1].trim();
+      cubeTotals[color] += number;
+      // console.log(number, color)
+    }
+    // debugCubeTotals(cubeTotals)
+    // debugCubeTotals(limits)
+
+    if (cubeTotals['red'] > smallestLimit['red']) { smallestLimit['red'] = cubeTotals['red']; } 
+    if (cubeTotals['green'] > smallestLimit['green']) { smallestLimit['green'] = cubeTotals['green']; }
+    if (cubeTotals['blue'] > smallestLimit['blue']) { smallestLimit['blue'] = cubeTotals['blue']; }
+  }
+  // console.log(pulls)
+  // console.log(validPulls)
+  return smallestLimit['red'] * smallestLimit['green'] * smallestLimit['blue']
+}
+
 
 export function part2(filename) {
   const lines = fileParser(filename, lineParserP2);
-  return 0;
+  // console.log(lines)
+  return sumNumbers(lines);
 }
 
 export function run() {
--- 2023/javascript/day-02/index.test.js
+++ 2023/javascript/day-02/index.test.js
@@ -3,31 +3,31 @@ import {
   part2,
 } from './index.js';
 
-const inputFilename = './day-01/input.txt';
-const testInputFilename = './day-01/test-input.txt';
+const inputFilename = './day-02/input.txt';
+const testInputFilename = './day-02/test-input.txt';
 
 describe('@/day-02/index.js', () => {
   describe('part1()', () => {
     it('part1 should pass', () => {
       const answer = part1(inputFilename);
-      expect(answer).toBe(0);
+      expect(answer).toBe(2439);
     });
 
     it('part1 should pass test input', () => {
       const answer = part1(testInputFilename);
-      expect(answer).toBe(0);
+      expect(answer).toBe(8);
     });
   });
 
   describe('part2()', () => {
     it('part2 should pass', () => {
       const answer = part2(inputFilename);
-      expect(answer).toBe(0);
+      expect(answer).toBe(63711);
     });
 
     it('part2 should pass test input', () => {
       const answer = part2(testInputFilename);
-      expect(answer).toBe(0);
+      expect(answer).toBe(2286);
     });
   });
 });
