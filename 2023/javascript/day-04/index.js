a0daa93f95f865855f471820e683b7de4274c49e
Dave Follett <davefollett@gmail.com>
2023-12-04 01:19:20 -0500

    2023 Day 04, parts 1 and 2 working.

--- 2023/javascript/day-04/index.js
+++ 2023/javascript/day-04/index.js
@@ -3,23 +3,99 @@
 import { performance } from 'node:perf_hooks';
 import Result from '../utils/result.js';
 import fileParser from '../utils/file-parser.js';
+import { sumNumbers } from '../utils/array.js';
 
 function lineParserP1(line) {
-    return line;
+  let numWinners = 0;
+  const winningNumbers = line.split(': ')[1].split(' | ')[0].split(' ').filter((number) => number.length);
+  const elfNumbers = line.split(': ')[1].split(' | ')[1].split(' ').filter((number) => number.length);
+
+  winningNumbers.forEach((number) => {
+    if (elfNumbers.includes(number)) {
+      numWinners += 1;
+    }
+  })
+
+  let result = 0;
+  if (numWinners) {
+    result = 1;
+    if (numWinners > 1) {
+      for (let i = 0; i<numWinners-1; i += 1 ) {
+        result *= 2;
+      }
+    }
+  }
+  
+  // console.log(numWinners)
+  // const result = Math.pow(2, numWinners - 1) + 1;
+  // console.log(result)
+  // console.log(winningNumbers)
+  // console.log(elfNumbers)
+  return result;
 }
 
 export function part1(filename) {
   const lines = fileParser(filename, lineParserP1);
-  return 0;
+  return sumNumbers(lines);
 }
 
 function lineParserP2(line) {
-  return line;
+  const cardNumber = line.split(': ')[0].split(' ').filter((number) => number.length)[1];
+  const winningNumbers = line.split(': ')[1].split(' | ')[0].split(' ').filter((number) => number.length);
+  const elfNumbers = line.split(': ')[1].split(' | ')[1].split(' ').filter((number) => number.length);
+
+  let numWinners = 0;
+  winningNumbers.forEach((number) => {
+    if (elfNumbers.includes(number)) {
+      numWinners += 1;
+    }
+  })
+
+  return {
+    cardNumber,
+    winningNumbers,
+    elfNumbers,
+    numWinners,
+    instances: 1,
+  }
 }
 
 export function part2(filename) {
-  const lines = fileParser(filename, lineParserP2);
-  return 0;
+  const cardArray = fileParser(filename, lineParserP2);
+  const cards = {};
+
+  cardArray.forEach((card) => {
+    cards[card.cardNumber] = {
+      winningNumbers: card.winningNumbers,
+      elfNumbers: card.elfNumbers,
+      numWinners: card.numWinners,
+      instances: card.instances,
+    }
+  })
+
+// console.log(cards)
+
+  for (const num in cards) {
+    // console.log(cards[num])
+    // console.log(`processing card ${num}, ${cards[num].instances} times.`)
+    for (let i = 0; i < cards[num].instances; i += 1) {
+      // console.log(`processing card ${num} instance number ${i}`)
+      for (let w = 1; w <= cards[num].numWinners; w += 1 ) {
+        const index = (parseInt(num, 10) + w).toString();
+        cards[index].instances += 1;
+        // console.log(index)
+      }
+    }
+  }
+
+  let result = 0;
+  for (const num in cards) {
+    result += cards[num].instances;
+  }
+
+  // for (let card = 0;)
+  // console.log(cards)
+  return result;
 }
 
 export function run() {
--- 2023/javascript/day-04/index.test.js
+++ 2023/javascript/day-04/index.test.js
@@ -10,24 +10,24 @@ describe('@/day-04/index.js', () => {
   describe('part1()', () => {
     it('part1 should pass', () => {
       const answer = part1(inputFilename);
-      expect(answer).toBe(0);
+      expect(answer).toBe(21919);
     });
 
     it('part1 should pass test input', () => {
       const answer = part1(testInputFilename);
-      expect(answer).toBe(0);
+      expect(answer).toBe(13);
     });
   });
 
   describe('part2()', () => {
     it('part2 should pass', () => {
       const answer = part2(inputFilename);
-      expect(answer).toBe(0);
+      expect(answer).toBe(9881048);
     });
 
     it('part2 should pass test input', () => {
       const answer = part2(testInputFilename);
-      expect(answer).toBe(0);
+      expect(answer).toBe(30);
     });
   });
 });
