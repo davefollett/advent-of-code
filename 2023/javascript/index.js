a2f42ca2631f78e1cca66a82bedfaa4d592b6ac7
Dave Follett <davefollett@gmail.com>
2023-12-04 23:30:31 -0500

    2023 Day 05 - stubbed in.

--- 2023/javascript/index.js
+++ 2023/javascript/index.js
@@ -4,6 +4,7 @@ import { run as day01Run } from './day-01/index.js';
 import { run as day02Run } from './day-02/index.js';
 import { run as day03Run } from './day-03/index.js';
 import { run as day04Run } from './day-04/index.js';
+import { run as day05Run } from './day-05/index.js';
 
 const PORT = 8080;
 
@@ -26,6 +27,7 @@ const sidebar = `
   <a class="link" href="/day-02">Day 02</a>
   <a class="link" href="/day-03">Day 03</a>
   <a class="link" href="/day-04">Day 04</a>
+  <a class="link" href="/day-05">Day 05</a>
 `;
 
 app.listen(PORT, () => {
@@ -54,5 +56,10 @@ app.get('/day-03', (req, res) => {
 
 app.get('/day-04', (req, res) => {
   const results = day04Run();
+  res.send(template(formatResult(results), sidebar));
+});
+
+app.get('/day-05', (req, res) => {
+  const results = day05Run();
   res.send(template(formatResult(results), sidebar));
 });
