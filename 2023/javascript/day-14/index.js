d2cf16b63c9585fdff26fc3ac4fa8c2bda3133ea
Dave Follett <davefollett@gmail.com>
2023-12-21 22:40:43 -0500

    2023 Day 14 Part 1 - slight refactor but the solution is still too slow.

--- 2023/javascript/day-14/index.js
+++ 2023/javascript/day-14/index.js
@@ -4,55 +4,28 @@ import { performance } from 'node:perf_hooks';
 import Result from '../utils/result.js';
 import Grid from '../utils/grid.js';
 
-// function processGrid(grid) {
-//   let didGridChange = false;
-//   for (let row = 0; row < grid.numRows; row += 1) {
-//     for (let col = 0; col < grid.numCols; col += 1) {
-//       grid.moveTo({row, col});
-//       if (grid.at() === 'O' && grid.peekUp() === '.') {
-//         grid.changeAt({ row: row + 1, col});
-//         didGridChange = true;
-//       }
-//     }
-//   }
-//   return didGridChange;
-// }
-
 export function part1(filename) {
   const grid = new Grid({ gridFilename: filename });
-  let didGridChange = false;
-  // console.log(grid.raw + '\n');
-  do {
-    didGridChange = false;
-    for (let row = 0; row < grid.numRows; row += 1) {
-      for (let col = 0; col < grid.numCols; col += 1) {
-    // for (let row = 0; row < 5; row += 1) {
-    //   for (let col = 0; col < grid.numCols; col += 1) {
-        const currentValue = grid.moveTo({row, col});
-        const { location: upLocation, value: upValue } = grid.peekUp();
-        // console.log(`at=${currentValue} up=${upValue}`)
-        // console.log('current location', grid.currentLocation, currentValue)
-        // console.log('up location', upLocation, upValue)
-        if (currentValue === 'O' && upValue === '.') {
-          grid.changeAt(upLocation, 'O');
-          grid.changeAt({row, col}, '.');
-          didGridChange = true;
-          // console.log('grid changed')
-        }
-      }
-      // console.log('\n')
-    }
-    // console.log(grid.raw + '\n');
-  } while (didGridChange);
-
-  // console.log(grid.raw + '\n');
-
   let result = 0;
+
   for (let row = 0; row < grid.numRows; row += 1) {
     for (let col = 0; col < grid.numCols; col += 1) {
-      const currentValue = grid.moveTo({row, col});
-      if (currentValue === 'O') {
-        result += grid.numRows - grid.currentLocation.row;
+      grid.moveTo({row, col});
+      if (grid.at() === 'O') {
+        let keepRolling = true;
+        do {
+          const { location: upLocation, value: upValue } = grid.peekUp();
+          if (upValue === '.') {
+            grid.changeAt(upLocation, 'O');
+            grid.changeAt(grid.currentLocation, '.');
+            grid.moveUp();
+          } else {
+            keepRolling = false;
+            if (grid.at() === 'O') {
+              result += grid.numRows - grid.currentLocation.row;
+            }
+          }
+        } while (keepRolling);
       }
     }
   }
